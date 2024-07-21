914a852c5d1d93c9051abf006d3d7e564ee4ef77
Yui ぉー <septianstalberts@gmail.com>
2024-07-21 22:45:55 +0700

    Update Home.svelte

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -10,15 +10,27 @@
 
 <NavBar isFixed={true} />
 
-<body>
-    <audio id="myAudio" autoplay>
-        <source src="ssstik.io_1721576340003.mp3" type="audio/mpeg">
-        Your browser does not support the audio element.
-    </audio>
+  <div class="music-player">
+            <audio controls id="audio-player">
+              <source src="https://k.top4top.io/m_30965f8680.m4a" type="audio/mpeg">
+              Your browser does not support the audio element.
+          </audio>
+            <div class="controls">
+                <button onclick="togglePlayPause()" id="play-pause-button">
+                  <i class="fas fa-play"></i>
+              </button>
+                <div class="progress-bar">
+                    <input type="range" min="0" max="100" value="0" step="0.1" id="progress-bar">
+                </div>
+            </div>
+            <div id="duration-info">
+                <span id="current-time">0:00</span> / <span id="total-duration">0:00</span>
+            </div>
+        </div>
+    </div>
 
 <div class="flex flex-col items-center justify-center h-screen flex-wrap">
   <img class="w-40 h-40 rounded-full" src={profile} alt="" loading="lazy" />
   <h1 class="mt-5 text-4xl text-white font-bold">Chyper Bot</h1>
   <SocialMedia />
 </div>
-</body>
