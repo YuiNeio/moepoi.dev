28d0599d1630da6fed7554a77b207f08ce2f0a81
Yui ぉー <septianstalberts@gmail.com>
2024-07-21 22:47:54 +0700

    Update Home.svelte

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -8,7 +8,13 @@
   <link rel="preload" href="{profile}" as="image">
 </svelte:head>
 
-<NavBar isFixed={true} />
+<NavBar isFixed={true} />           
+
+<div class="flex flex-col items-center justify-center h-screen flex-wrap">
+  <img class="w-40 h-40 rounded-full" src={profile} alt="" loading="lazy" />
+  <h1 class="mt-5 text-4xl text-white font-bold">Chyper Bot</h1>
+  <SocialMedia />
+</div>
 
   <div class="music-player">
             <audio controls id="audio-player">
@@ -19,18 +25,3 @@
                 <button onclick="togglePlayPause()" id="play-pause-button">
                   <i class="fas fa-play"></i>
               </button>
-                <div class="progress-bar">
-                    <input type="range" min="0" max="100" value="0" step="0.1" id="progress-bar">
-                </div>
-            </div>
-            <div id="duration-info">
-                <span id="current-time">0:00</span> / <span id="total-duration">0:00</span>
-            </div>
-        </div>
-    </div>
-
-<div class="flex flex-col items-center justify-center h-screen flex-wrap">
-  <img class="w-40 h-40 rounded-full" src={profile} alt="" loading="lazy" />
-  <h1 class="mt-5 text-4xl text-white font-bold">Chyper Bot</h1>
-  <SocialMedia />
-</div>
