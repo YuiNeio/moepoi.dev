f04949998186bc51a8e13f8b61e328265ff9f90f
Yui ぉー <septianstalberts@gmail.com>
2024-07-21 21:36:26 +0700

    Update Home.svelte

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -12,6 +12,6 @@
 
 <div class="flex flex-col items-center justify-center h-screen flex-wrap">
   <img class="w-40 h-40 rounded-full" src={profile} alt="" loading="lazy" />
-  <h1 class="mt-5 text-4xl text-white font-bold">Moepoi</h1>
+  <h1 class="mt-5 text-4xl text-white font-bold">Chyper | WhatsApp Bot</h1>
   <SocialMedia />
 </div>
