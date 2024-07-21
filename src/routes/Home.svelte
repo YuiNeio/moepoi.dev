811d9fc4b62d3e8ef47c237f9ea7cfec31a33ad9
Yui ぉー <septianstalberts@gmail.com>
2024-07-21 22:39:52 +0700

    Update Home.svelte

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -10,8 +10,15 @@
 
 <NavBar isFixed={true} />
 
+<body>
+    <audio id="myAudio" autoplay>
+        <source src="chyper.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+    </audio>
+
 <div class="flex flex-col items-center justify-center h-screen flex-wrap">
   <img class="w-40 h-40 rounded-full" src={profile} alt="" loading="lazy" />
   <h1 class="mt-5 text-4xl text-white font-bold">Chyper Bot</h1>
   <SocialMedia />
 </div>
+</body>
