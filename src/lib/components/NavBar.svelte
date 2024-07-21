92af1c7917a43d7372f5a42554ad12211afc7698
Yui ぉー <septianstalberts@gmail.com>
2024-07-21 21:49:34 +0700

    Update NavBar.svelte

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -7,6 +7,5 @@
 <nav class="{isFixed ? 'fixed' : ''} top-0 left-0 w-full z-50 flex justify-center space-x-4 p-4">
   <Link to="/" class='text-white hover:underline'>Home</Link>
   <Link to="/projects" class='text-white hover:underline'>Projects</Link>
-  <Link to="/resume" class='text-white hover:underline'>Resume</Link>
   <Link to="/contact" class='text-white hover:underline'>Contact</Link>
 </nav>
